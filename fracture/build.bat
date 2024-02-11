b0333dcc5ba3e984c5861131745ab5d2311dd016
Aditya Rajagopal <adi.raj465@gmail.com>
2024-02-11 09:21:54 -0800

    fixes to the tasks.json and update build files to output compile_flags.txt

--- fracture/build.bat
+++ fracture/build.bat
@@ -12,10 +12,15 @@ REM echo "Files: " %cFileNames%
 SET assembly=fracture
 SET comilerFlags=-g -shared -Wvarargs -Wall -Werror
 REM -Wall -Werror
-SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
+SET includeFlags=-Isrc -I%VULKAN_SDK%\Include
 SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
 SET defines=-D_DEBUG -DFR_EXPORT -D_CRT_SECURE_NO_WARNINGS
 
+ECHO %VULKAN_SDK%
 
 ECHO "Buildin %assembly%..."
 clang %cFileNames% %comilerFlags% -o ../bin/%assembly%.dll %defines% %includeFlags% %linkerFlags%
+
+REM "Writing the compile_flags.txt file"
+ECHO "Writing the compile_flags.txt file"
+echo %includeFlags% %defines% %comilerFlags% | sed -e "s/-I/-I\\/g" -e "s/ /\n/g" -e "s/-I\\C/-IC/g" -e "s/-I\\..\\/-I..\\/g"> compile_flags.txt
--- testbed/build.bat
+++ testbed/build.bat
@@ -12,10 +12,13 @@ REM echo "Files: " %cFileNames%
 SET assembly=testbed
 SET comilerFlags=-g
 REM -Wall -Werror
-SET includeFlags=-Isrc -I../fracture/src
+SET includeFlags=-Isrc -I..\fracture\src
 SET linkerFlags=-L../bin/ -lfracture.lib
 SET defines=-D_DEBUG -DFR_IMPORT
 
-
 ECHO "Buildin %assembly%...."
 clang %cFileNames% %comilerFlags% -o ../bin/%assembly%.exe %defines% %includeFlags% %linkerFlags%
+
+REM "Writing the compile_flags.txt file"
+ECHO "Writing the compile_flags.txt file"
+echo %includeFlags% %defines% %comilerFlags% | sed -e "s/-I/-I\\/g" -e "s/ /\n/g" -e "s/-I\\C/-IC:/g" -e "s/-I\\..\\/-I..\\/g" > compile_flags.txt
